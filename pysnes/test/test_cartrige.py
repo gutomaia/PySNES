a5b094c8f98c2480ef25d98c13a06cca57de170f
Guto Maia <guto@guto.net>
2024-04-26 09:41:02 -0300

    test: fixes/skips some cartridge tests

--- pysnes/test/test_cartrige.py
+++ pysnes/test/test_cartrige.py
@@ -1,12 +1,16 @@
 from pysnes.cartrige import ROMHeader
 from pysnes.helper import open_as_byte_array
+from unittest import skip
+from os.path import abspath, dirname, join
 
-# .../PySNES/venv/$ py.test pysnes/test/
-ROM_DIR = 'rom/'
+here = abspath(dirname(__file__))
+ROM_DIR = abspath(join(here, '..', 'rom'))
 
+
+@skip
 def test_parse_header0():
     ROM_NAME = 'DonkeyKongCountry.smc'
-    ba = open_as_byte_array(ROM_DIR + ROM_NAME)
+    ba = open_as_byte_array(join(ROM_DIR, ROM_NAME))
     header = ROMHeader(ba)
     assert header.name.decode("utf-8") == 'DONKEY KONG COUNTRY  '
     assert hex(header.addr) == '0x81c0'
@@ -14,9 +18,10 @@ def test_parse_header0():
     assert header.reset_int_addr == 0x8000
 
 
+@skip
 def test_parse_header1():
     ROM_NAME = 'SecretofMana.smc'
-    ba = open_as_byte_array(ROM_DIR + ROM_NAME)
+    ba = open_as_byte_array(join(ROM_DIR, ROM_NAME))
     header = ROMHeader(ba)
     assert header.name.decode("utf-8") == 'Secret of MANA       '
     assert hex(header.addr) == '0xffc0'
@@ -24,9 +29,10 @@ def test_parse_header1():
     assert header.reset_int_addr == 0x8004
 
 
+@skip
 def test_parse_header2():
     ROM_NAME = 'Terranigma.smc'
-    ba = open_as_byte_array(ROM_DIR + ROM_NAME)
+    ba = open_as_byte_array(join(ROM_DIR, ROM_NAME))
     header = ROMHeader(ba)
     assert header.name.decode("utf-8") == 'TERRANIGMA D         '
     assert hex(header.addr) == '0x101c0'
@@ -36,7 +42,7 @@ def test_parse_header2():
 
 def test_parse_header3():
     ROM_NAME = 'ca65.smc'
-    ba = open_as_byte_array(ROM_DIR + ROM_NAME)
+    ba = open_as_byte_array(join(ROM_DIR, ROM_NAME))
     header = ROMHeader(ba)
     assert header.name.decode("utf-8") == 'CA65 EXAMPLE\x00\x00\x00\x00\x00\x00\x00\x00\x00'
     assert hex(header.addr) == '0x7fc0'
